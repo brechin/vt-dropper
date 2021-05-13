4965f1c90840e1457f45479f9b317deec780b381
Jason Brechin <brechinj@gmail.com>
2021-05-13 07:41:22 -0500

    Initial commit

---
+++ DropperMod.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using VoxelTycoon;
+using VoxelTycoon.Buildings;
+using VoxelTycoon.Game;
+using VoxelTycoon.Modding;
+
+namespace Dropper
+{
+    public class DropperMod : Mod
+    {
+        private Hotkey _dropperHotkey = new Hotkey(KeyCode.Tilde);
+        
+        protected override void OnGameStarted()
+        {
+            
+        }
+
+        protected override void OnUpdate()
+        {
+            if (LazyManager<InputManager>.Current.GetKeyDown(_dropperHotkey))
+            {
+                var building = GameUI.Current.BuildingUnderCursor;
+                Debug.Log(building);
+                Debug.Log(building.IsBuilt);
+            }
+            
+        }
+    }
+}
