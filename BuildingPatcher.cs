3aaf8d4e2fd2164333279bc8c0fa24968ab34bc8
Jason Brechin <brechinj@gmail.com>
2021-06-18 16:27:09 -0500

    Support copying recipes to copied buildings

---
+++ BuildingPatcher.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using VoxelTycoon;
+using VoxelTycoon.Buildings;
+using VoxelTycoon.Tools.Builder;
+
+namespace Dropper
+{
+    [HarmonyPatch(typeof(BuilderTool), "Rotate")]
+    public class BuildingPatcher
+    {
+        private static readonly Logger _logger = new Logger<BuildingPatcher>();
+
+        private static Building GetGhost(BuilderTool toolInstance)
+        {
+            var toolAccessor = Traverse.Create(toolInstance);
+            return (Building) toolAccessor.Property("Ghost").GetValue();
+        }
+
+        private static void SetGhost(BuilderTool toolInstance, Building newGhost)
+        {
+            var toolAccessor = Traverse.Create(toolInstance);
+            toolAccessor.Property("Ghost").SetValue(newGhost);
+        }
+        
+        static bool Prefix(BuilderTool __instance)
+        {
+            _logger.Log($"In Prefix for {__instance}");
+            var instanceAccessor = Traverse.Create(__instance);
+
+            try
+            {
+                var ghost = instanceAccessor.Property("Ghost").GetValue<Building>();
+                _logger.Log($"Ghost: {ghost}");
+                if (ghost is null) return false;
+                var rotationAccessor = instanceAccessor.Field("_rotation");
+                var rotation = (BuildingRotation) rotationAccessor.GetValue();
+                rotation = rotation.Add(BuildingRotation.Rotate90);
+                rotationAccessor.SetValue(rotation);
+                _logger.Log($"Set rotation to {rotation}");
+                ghost.SetRotation(rotation);
+                instanceAccessor.Property("Ghost").SetValue(ghost);
+                _logger.Log("Set ghost rotation");
+            }
+            catch (AmbiguousMatchException)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- DropperMod.cs
+++ DropperMod.cs
@@ -1,32 +1,41 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using UnityEngine;
 using VoxelTycoon;
 using VoxelTycoon.Buildings;
 using VoxelTycoon.Game;
 using VoxelTycoon.Modding;
+using VoxelTycoon.Researches;
 using VoxelTycoon.Tools.Builder;
 using VoxelTycoon.Tools.TrackBuilder;
+using VoxelTycoon.Tracks.Conveyors;
+using VoxelTycoon.Tracks.Pipes;
 using VoxelTycoon.Tracks.Rails;
 using VoxelTycoon.Tracks.Roads;
 using VoxelTycoon.UI;
 using Logger = VoxelTycoon.Logger;
+using Object = UnityEngine.Object;
 
 namespace Dropper
 {
     // Subclass VoxelTycoon.Modding.Mod as the integration point with the game
     public class DropperMod : Mod
     {
-        // Set the hotkey for activating the dropper
-        private readonly Hotkey _dropperHotkey = new Hotkey(KeyCode.F2);
-        private readonly Hotkey _qHotkey = new Hotkey(KeyCode.F4);
         private static bool _qShown;
 
+        // Set the hotkey for activating the dropper
+        private readonly Hotkey _dropperHotkey = new(KeyCode.F2);
+
         // Set up a logger that outputs nicely
-        readonly Logger _logger = new Logger<DropperMod>();
+        private readonly Logger _logger = new Logger<DropperMod>();
+        private readonly Hotkey _qHotkey = new(KeyCode.F4);
 
         protected override void Initialize()
         {
             // Initialize is called when your mod .dll is loaded
-
+            var harmony = new Harmony("com.vtdropper.patch");
+            harmony.PatchAll();
             _logger.Log("Initialized!");
         }
 
@@ -34,18 +43,17 @@ namespace Dropper
         protected override void OnGameStarted()
         {
             // OnGameStarted is called when a world is loaded with your mod turned on
-
             _logger.Log("Dropper Mod is here!");
         }
 
-        private void CancelCurrentTool()
+        private static void CancelCurrentTool()
         {
             // Taken from VoxelTycoon.UI.UIManager.UpdateTool
             var uiManager = UIManager.Current;
             uiManager.SetTool(null, true);
         }
 
-        private void ShowToolQuery()
+        private static void ShowToolQuery()
         {
             GUIHelper.Draw(() =>
             {
@@ -59,66 +67,140 @@ namespace Dropper
         {
             // OnUpdate is called on every game update
 
-            // Tool query window (useful for dev/debugging)
+            // Toggle Tool query window (useful for dev/debugging)
             if (LazyManager<InputManager>.Current.GetKeyDown(_qHotkey))
                 _qShown = !_qShown;
 
             if (_qShown)
                 ShowToolQuery();
-                
-            // If user is pressing our key
-            if (LazyManager<InputManager>.Current.GetKeyDown(_dropperHotkey))
+
+            // Return if user is not pressing the "dropper" key
+            if (!LazyManager<InputManager>.Current.GetKeyDown(_dropperHotkey)) return;
+
+            // Cancel current tool, if one is in use
+            CancelCurrentTool();
+
+            // Inspect the state of the UI to get the building under the cursor
+            var building = GameUI.Current.BuildingUnderCursor;
+            // TODO: Maybe GetGameObjectUnderPointer?
+
+            if (building == null)
+                return;
+            _logger.Log($"Selected building: {building}");
+
+            // Return if the building doesn't belong to the player's company
+            if (building.Company != Company.Current) return;
+
+            // Get the BuilderTool to begin placing a copy of the selected building 
+            var builderTool = GetTool(building);
+
+            // Set the tool active in the UI
+            UIManager.Current.SetTool(builderTool);
+
+            // Create accessor for protected/private members
+            var toolAccessor = Traverse.Create(builderTool);
+
+            // Set rotation of copy same as original building
+            if (builderTool is BuilderTool tool)
             {
-                // Cancel current tool, if one is in use
-                CancelCurrentTool();
-                
-                // Inspect the state of the UI to get the building under the cursor
-                var building = GameUI.Current.BuildingUnderCursor;
-                // TODO: Maybe GetGameObjectUnderPointer?
+                tool.Rotation = building.Rotation;
+            }
 
-                if (building == null)
-                    return;
-                _logger.Log(building.ToString());
-
-                // If the building belongs to the player's company
-                if (building.Company == Company.Current)
+            switch (building)
+            {
+                case Warehouse warehouse:
                 {
-                    var builderTool = GetTool(building);
-                    // Get the BuilderTool to begin placing a copy of the selected building 
-                    // BuilderTool builderTool = BuilderToolManager.Current.GetTool(buildingRecipe);
-                    _logger.Log("Tool: " + builderTool);
+                    var ghost = (StorageNetworkBuilding) toolAccessor.Property("Ghost").GetValue();
+                    if (warehouse.Storage != null)
+                    {
+                        _logger.Log("Copying storage");
+                        if (ghost != null)
+                        {
+                            ghost.Storage = warehouse.Storage.Instantiate(false);
+                            toolAccessor.Property("Ghost").SetValue(ghost);
+                        }
+                        // ((Warehouse) ((StorageNetworkBuildingBuilderTool) builderTool).Recipe.Building).Storage =
+                        //     warehouse.Storage.Instantiate(false);
+                    }
+                    else
+                    {
+                        _logger.Log("Null storage?");
+                    }
+            
+                    break;
+                }
+                case Device device:
+                {
+                    var ghost = (Device) toolAccessor.Property("Ghost").GetValue();
+                    if (ghost != null)
+                    {
+                        Traverse.Create(ghost).Field("_recipe").SetValue(device.Recipe);
+                        toolAccessor.Property("Ghost").SetValue(ghost);
+                    }
 
-                    // Set the tool as the currently-selected tool
-                    UIManager.Current.SetTool(builderTool);
-                    _logger.Log("Tool activated");
+                    break;
                 }
             }
+
+            _logger.Log("Tool activated");
         }
 
         private ITool GetTool(Building building)
         {
-            // Get the appropriate build tool for the selected building
+            // Map of "special" types of buildings that need specific tools
+            var toolMapper = new Dictionary<Type, Func<Building, ITool>>
+            {
+                {typeof(RailStation), b => new RailStationBuilderTool((RailStation) b)},
+                {typeof(Road), b => new RoadBuilderTool((Road) b)},
+                {typeof(Rail), b => new RailBuilderTool((Rail) b)},
+                {typeof(Pipe), b => new PipeBuilderTool((Pipe) b)},
+                {typeof(Conveyor), b => new ConveyorBuilderTool((Conveyor) b)}
+            };
+
+            // Get the appropriate build tool for the selected building if it's one of the "special" types
             var buildingType = building.GetType();
-            if (buildingType == typeof(RailStation))
+            if (toolMapper.TryGetValue(buildingType, out var toolFactory))
             {
-                _logger.Log("Returning Station");
-                return new RailStationBuilderTool((RailStation) building);
-            }
-            if (buildingType == typeof(Road))
-            {
-                _logger.Log("Returning RoadBuilderTool");
-                return new RoadBuilderTool((Road) building);
+                _logger.Log("Returning mapped type " + toolFactory);
+                return toolFactory(building);
             }
 
-            if (buildingType == typeof(Rail))
-            {
-                _logger.Log("Returning RailBuilderTool");
-                return new RailBuilderTool((Rail) building);
-            }
+            // Otherwise, fetch the building recipe by its asset_id
+            var buildingRecipe = BuildingRecipeManager.Current.Get(building.AssetId);
+            buildingRecipe.Building = UnityEngine.Object.Instantiate<Building>(building);
+
+            var builderTool = BuilderToolManager.Current.GetTool(buildingRecipe);
             
-            // Fetch the building recipe by its asset_id
-            BuildingRecipe buildingRecipe = BuildingRecipeManager.Current.Get(building.AssetId);
-            BuilderTool builderTool = BuilderToolManager.Current.GetTool(buildingRecipe);
+            // Get current tool Recipe
+            _logger.Log($"Recipe: {builderTool.Recipe}");
+            
+            // Create a copy of the current building to replicate settings
+            var newBuilding = UnityEngine.Object.Instantiate<Building>(building);
+            
+            // Set storage if it's a warehouse
+            if (building is Warehouse warehouse)
+            {
+                Warehouse newWarehouse = (Warehouse) UnityEngine.Object.Instantiate<Warehouse>(warehouse);
+                newBuilding = newWarehouse;
+                _logger.Log($"Old storage: {warehouse.Storage} New: {newWarehouse.Storage}");
+                if (warehouse.Storage != null)
+                    newWarehouse.Storage = warehouse.Storage.Instantiate(false);
+            }
+
+            // Set recipe if it's a factory
+            if (building is Device device)
+            {
+                _logger.Log($"Old recipe: {device.Recipe} New: {((Device) newBuilding).Recipe}");
+                Traverse.Create(newBuilding).Field("_recipe").SetValue(device.Recipe);
+            }
+
+            // TODO: Copy research to new Lab
+            
+            _logger.Log("New building: " + newBuilding);
+            
+            // Set recipe building to one with duplicated settings
+            builderTool.Recipe.Building = newBuilding;
+
             return builderTool;
         }
     }
